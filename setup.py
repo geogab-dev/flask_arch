47311020fa8165ac8d0a26892a3bb069900bd18f
George Gabriel <57449323+Geo-Gabriel@users.noreply.github.com>
2021-01-31 11:38:36 -0300

    updated

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import os
 from setuptools import setup, find_packages
 
 
-def read(fname):
-    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+def read(file_name):
+    return codecs.open(os.path.join(os.path.dirname(__file__), file_name)).read()
 
 
 PACKAGE = "flask_arch"
 NAME = "flask_arch"
-DESCRIPTION = "Gerador de templates para aplicações Flask"
+DESCRIPTION = "Template generator for Flask applications."
 AUTHOR = "George Gabriel"
-AUTHOR_EMAIL = "geoquimics@gmail.com"
+AUTHOR_EMAIL = "geogab.dev@gmail.com"
 URL = "https://github.com/Geo-Gabriel/flask_arch"
 VERSION = __import__(PACKAGE).__version__
 
@@ -21,7 +21,7 @@ setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=read("README.md"),
+    long_description=read("README.txt"),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="MIT",
